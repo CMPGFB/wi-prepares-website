388db62d3a244eaa24e957cbf66aa747ab1501de
C.M.P. <36182708+CMPGFB@users.noreply.github.com>
2025-06-27 21:23:50 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+});
+
+// Toggle checkbox state if used
+function toggleCheck(el) {
+  el.classList.toggle('checked');
+}
